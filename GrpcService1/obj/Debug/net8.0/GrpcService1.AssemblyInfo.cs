689c43571aa721b4223956f651e7f547b84dec5c
thuan <nguyenchithuan55@gmail.com>
2024-07-23 11:55:51 +0700

    fix

--- GrpcService1/obj/Debug/net8.0/GrpcService1.AssemblyInfo.cs
+++ GrpcService1/obj/Debug/net8.0/GrpcService1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrpcService1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b140dcd0beae8ff504e988cec4b127d068e8e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67cd9870a1a54c695dd30c287e9b3f4da66abde2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrpcService1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrpcService1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
